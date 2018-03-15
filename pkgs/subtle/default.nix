cc3a8b22cda0bfd61481aa095c587739199418f3
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2018-03-15 19:15:36 +0100

    added subtle pkg

---
+++ pkgs/subtle/default.nix
@@ -0,0 +1,34 @@
+{ pkgs, fetchhg, stdenv, bundlerEnv, ruby, ... }:
+
+let
+  gems = bundlerEnv {
+    name = "ruby-env";
+    inherit ruby;
+    gemdir = ./.;
+  };
+in
+stdenv.mkDerivation rec {
+  name = "subtle-${version}";
+  version = "0.11";
+
+  src = fetchhg {
+    url = "http://hg.subforge.org/subtle";
+    rev = "0.11";
+    sha256 = "16bwp04ylvrhz34zi69h3qzwrhx9qqmqlwl63d4c7byc4xwpk036";
+  };
+
+  patchPhase = ''
+  substitute ./Rakefile ./Rakefile \
+    --replace "fail(\"Ruby 1.9.0 or higher required\")" "" \
+    --replace "-Wall" "" \
+    --replace "sitelibdir" "vendorlibdir"
+  '';
+  buildInputs = with pkgs; [  ];
+  propagatedBuildInputs = with pkgs; [ pkgconfig gems gems.wrappedRuby xorg.libXrandr xorg.libXft xorg.libXpm xorg.libXinerama xorg.libXtst ];
+  buildPhase = ''
+  rake default xtest=false debug=yes destdir=$out prefix= extdir=$out/subtle
+  '';
+  installPhase = "rake install";
+
+  doCheck = false;
+}
---
+++ pkgs/subtle/gemset.nix
@@ -0,0 +1,10 @@
+{
+  rake = {
+    source = {
+      remotes = ["http://rubygems.org"];
+      sha256 = "190p7cs8zdn07mjj6xwwsdna3g0r98zs4crz7jh2j2q5b0nbxgjf";
+      type = "gem";
+    };
+    version = "12.3.0";
+  };
+}
