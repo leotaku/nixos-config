eb20a18c557a365c442cfbb277df1deeab5d44fd
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2018-03-22 23:43:12 +0100

    fixed openvpn paths

--- plugables/openvpn/serverlist.nix
+++ plugables/openvpn/serverlist.nix
@@ -3,7 +3,7 @@
   # Add OpenVPN servers, currently only works on one system
   services.openvpn.servers = {
     mullvadAT = { 
-      config = '' config /home/leo/openvpn/mullvad_at.conf '';
+      config = builtins.readFile ../../private/openvpn/mullvad_at.conf;
       updateResolvConf = true;
     };
   };
