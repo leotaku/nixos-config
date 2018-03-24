3c45640df3a6ca3ba8f5b5f3f79f37783bd57fad
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2018-03-24 08:57:04 +0100

    added new openvpn server

--- plugables/openvpn/serverlist.nix
+++ plugables/openvpn/serverlist.nix
@@ -3,12 +3,18 @@
   # Add OpenVPN servers, currently only works on one system
   services.openvpn.servers = {
     mullvadAT = { 
-      config = builtins.readFile ../../private/openvpn/mullvad_at.conf;
+      config = builtins.readFile ../../private/openvpn/mullvadAT/mullvad_at.conf;
       updateResolvConf = true;
+      autoStart = false;
+    };
+    mullvadHU = { 
+      config = builtins.readFile ../../private/openvpn/mullvadHU/mullvad_hu.conf;
+      updateResolvConf = true;
+      autoStart = false;
     };
   };
 
   powerManagement.resumeCommands = ''
-    ${pkgs.systemd}/bin/systemctl restart openvpn-mullvadAT.service
+    ${pkgs.bash}/bin/bash -c "${pkgs.systemd}/bin/systemctl restart `${pkgs.systemd}/bin/systemctl --plain list-unit-files | ${pkgs.gnugrep}/bin/grep 'enabled *$' | ${pkgs.gnugrep}/bin/grep -o 'openvpn[^ ]*`"
   '';
 }
