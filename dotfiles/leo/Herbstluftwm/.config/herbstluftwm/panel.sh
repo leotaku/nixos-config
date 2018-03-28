cda1879a3a7504598b50e1d6d27323104c23eeef
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2018-03-28 11:25:13 +0200

    updated dotfiles

--- dotfiles/leo/Herbstluftwm/.config/herbstluftwm/panel.sh
+++ dotfiles/leo/Herbstluftwm/.config/herbstluftwm/panel.sh
@@ -17,7 +17,7 @@ fi
 x=${geometry[0]}
 y=${geometry[1]}
 panel_width=${geometry[2]}
-panel_height=16
+panel_height=36
 font="-*-fixed-medium-*-*-*-12-*-*-*-*-*-*-*"
 bgcolor=$(hc get frame_border_normal_color)
 selbg=$(hc get window_border_active_color)
