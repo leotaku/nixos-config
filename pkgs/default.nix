7c2ac7a12ff5a12fae3f4e3ec8dbd147a7408a0a
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2018-06-25 22:56:31 +0200

    removed unused pkgs, alphabetical order

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,47 +2,40 @@ self: super:
 
 {
   # Custom packages
-  instant-markdown-d = (super.callPackage ./instant-markdown-d/default.nix {}).package;
+  athame-zsh = super.callPackage ./zsh/athame-zsh.nix {};
+  athameVim = super.callPackage ./vim/athameVim.nix {};
+  broadcom-rpi3-extra = super.callPackage ./raspberry/broadcom_driver.nix {}; # TODO move back to rpi system
+  catimg = super.callPackage ./catimg/default.nix {};
+  dmenu3 = super.callPackage ./dmenu/dmenu3.nix {};
+  eventd = super.callPackage ./eventd/default.nix {};
+  farfetch = super.callPackage ./farfetch/default.nix {};
+  gohufont-ttf = super.callPackage ./gohufont-ttf/default.nix {};
+  gotop = super.callPackage ./gotop/default.nix {};
   gtop =  (super.callPackage ./gtop/default.nix {}).package;
-  customVimPlugins = super.callPackage ./vim/customPlugins.nix {};
-  leovim = super.callPackage ./neovim/leovim.nix {};
+  instant-markdown-d = (super.callPackage ./instant-markdown-d/default.nix {}).package;
+  interrobang = super.callPackage ./interrobang/default.nix {};
+  mullvad-gtk = super.callPackage ./mullvad/gtk-client.nix {};
+  num9menu = super.callPackage ./9menu/default.nix {};
+  seashells = super.callPackage ./seashells/default.nix {};
+  subtle = super.callPackage ./subtle/default.nix {};
+  torrench = super.callPackage ./torrench/default.nix {};
+  windowchef = super.callPackage ./windowchef/default.nix {};
+  xgetres = super.callPackage ./xgetres/default.nix {};
+
+  # Customized packages
   leoVim = super.callPackage ./vim/leoVim.nix {};
+  leovim = super.callPackage ./neovim/leovim.nix {};
+  ncmpcpp = super.ncmpcpp.override { visualizerSupport=true; outputsSupport=true; clockSupport=true; };
+  num2bwm = super.callPackage ./2bwm/default.nix {};
+  oh-my-zsh-custom = super.callPackage ./zsh/oh-my-zsh.nix {};
   orgEmacs = super.callPackage ./emacs/orgmode.nix {};
   orgEmacsConfig = super.callPackage ./emacs/configuration-package.nix {};
-  athameVim = super.callPackage ./vim/athameVim.nix {};
-  windowchef = super.callPackage ./windowchef/default.nix {};
-  farfetch = super.callPackage ./farfetch/default.nix {};
-  customPythonPackages = super.callPackage ./python/customPackages.nix {};
-  mullvad-gtk = super.callPackage ./mullvad/gtk-client.nix {};
-  torrench = super.callPackage ./torrench/default.nix {};
-  broadcom-rpi3-extra = super.callPackage ./raspberry/broadcom_driver.nix {};
-  subtle = super.callPackage ./subtle/default.nix {};
-  eventd = super.callPackage ./eventd/default.nix {};
-  interrobang = super.callPackage ./interrobang/default.nix {};
-  athame-zsh = super.callPackage ./zsh/athame-zsh.nix {};
-  dmenu3 = super.callPackage ./dmenu/dmenu3.nix {};
-  urxvtWithExtensions = super.callPackage ./urxvt/default.nix {};
-  seashells = super.callPackage ./seashells/default.nix {};
-  xgetres = super.callPackage ./xgetres/default.nix {};
-  gotop = super.callPackage ./gotop/default.nix {};
-  num9menu = super.callPackage ./9menu/default.nix {};
-  catimg = super.callPackage ./catimg/default.nix {};
-  gohufont-ttf = super.callPackage ./gohufont-ttf/default.nix {};
-  #xero-figlet-fonts = super.callPackage ./xero-figlet-fonts/default.nix {};
-  #meson = super.callPackage ./meson/default.nix {};
-  # Not currently working
-  #howm = super.callPackage ./howm/default.nix {};
-  #cottage = super.callPackage ./cottage/default.nix {};
-  #gpick = super.callPackage ./gpick/default.nix {};
-  # Package Overrides
-  num2bwm = super.callPackage ./2bwm/default.nix {};
+  polybar = super.polybar.override { githubSupport = true; mpdSupport = true; };
+  sxiv = super.sxiv.override { conf = builtins.readFile /home/leo/nixos-config/files/config.h; }; # TODO fix path
   wmutils-core = super.callPackage ./wmutils/core.nix {};
   wmutils-opt = super.callPackage ./wmutils/opt.nix {};
-  ncmpcpp = super.ncmpcpp.override { visualizerSupport=true; outputsSupport=true; clockSupport=true; };
-  polybar = super.polybar.override { githubSupport = true; mpdSupport = true; };
-  sxiv = super.sxiv.override { conf = builtins.readFile /home/leo/nixos-config/files/config.h; };
-  oh-my-zsh-custom = super.callPackage ./zsh/oh-my-zsh.nix {};
-  #ranger = super.callPackage ./ranger/default.nix {};
-  # Global env for stupid programmes
-  #pythonForR = super.callPackage ../env/python/rSystem.nix {};
+
+  # Collections
+  customPythonPackages = super.callPackage ./python/customPackages.nix {};
+  customVimPlugins = super.callPackage ./vim/customPlugins.nix {};
 }
