e532d0cf0849365c8df88da7aecfc7f87b4286ae
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2018-08-29 01:48:50 +0200

    update ncm2-syntax

--- pkgs/vim/ncm2.nix
+++ pkgs/vim/ncm2.nix
@@ -5,7 +5,7 @@
     src = fetchFromGitHub {
       owner = "ncm2";
       repo = "ncm2";
-      rev = "f962aa0";
+      rev = "f962aa074a3bf4e7526beca21442e03987829d43";
       sha256 = "1swca2iyfwn90h3advgzb5ipwqpxg8r4r0qc7ryik2k2awapnjgf";
     };
   };
@@ -15,7 +15,7 @@
     src = fetchFromGitHub {
       owner = "roxma";
       repo = "nvim-yarp";
-      rev = "52317ce";
+      rev = "52317ced0e16f226f0d44878917d0b5f4657b4d4";
       sha256 = "1xj1n9x1nxjbbpp29x5kkwr0bxziwzn8n2b8z9467hj9w646zyrj";
     };
   };
@@ -25,7 +25,7 @@
     src = fetchFromGitHub {
       owner = "ncm2";
       repo = "ncm2-ultisnips";
-      rev = "15432d7";
+      rev = "15432d7933cfb855599442a67d6f39ddb706c737";
       sha256 = "0ixajh08fd5dgdz4h1sdxgiaind1nksk1d4lwyb6n4ijf672pms2";
     };
   };
@@ -35,7 +35,7 @@
     src = fetchFromGitHub {
       owner = "ncm2";
       repo = "ncm2-racer";
-      rev = "e3aec08";
+      rev = "e3aec0836ea1ff1b820e937f9c6463eb015fa784";
       sha256 = "03vd252qm6b3isd45jz7wah3p9sm73pf4gxngwsfb1hc1hn7c1cf";
     };
   };
@@ -45,7 +45,7 @@
     src = fetchFromGitHub {
       owner = "ncm2";
       repo = "ncm2-path";
-      rev = "875ae47";
+      rev = "875ae47e171abc2ba6710bb835727bed46d7b329";
       sha256 = "09vhggrb1nigr8p53gd9ibn3b84dh9yix2ndj2453wnq8ny9x2dc";
     };
   };
@@ -55,8 +55,8 @@
     src = fetchFromGitHub {
       owner = "ncm2";
       repo = "ncm2-syntax";
-      rev = "8007abf";
-      sha256 = "0w7gxy96cnvsx4qy7saan7sk7hmmpsn06qyhx72svjx8h78kz0ac";
+      rev = "b13180a39ec1be77d19ece47ed91deed9fca200b";
+      sha256 = "1rlycny5mivli02nnn8874pmr82qad393i4mrb08iqf8zrczvxd5";
     };
   };
   
@@ -65,7 +65,7 @@
     src = fetchFromGitHub {
       owner = "ncm2";
       repo = "ncm2-bufword";
-      rev = "86a92eb";
+      rev = "86a92eb3fb217f9ea1e93f890b7e5e0eb1671ca9";
       sha256 = "02f43rr9apgcjpz4ipnin4v3cvdlx931a0787x87iyr8a0aljg3y";
     };
   };
@@ -75,7 +75,7 @@
     src = fetchFromGitHub {
       owner = "ncm2";
       repo = "ncm2-tmux";
-      rev = "4f60ee1";
+      rev = "4f60ee1be57531295075d808e0006c83894096d1";
       sha256 = "1ihbm65b9bc0y068w6r0k8f9lsc3603npb55chcchpj7z75539yh";
     };
   };
