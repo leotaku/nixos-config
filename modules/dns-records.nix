aeb027397d5e5b9e919abe4fbd36212853bf4681
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2018-12-22 17:13:43 +0100

    Add "modules" directory and first module: update-dns-records

---
+++ modules/dns-records.nix
@@ -0,0 +1,32 @@
+{ lib, pkgs, config, ... }:
+with lib;                      
+let
+  cfg = config.services.dns-records-update;
+
+  createServices =
+    (mapAttrs' (n: url:
+      nameValuePair ("dns-records-update-${n}")
+      ({
+        wantedBy = [ "multi-user.target" ];
+        startAt = cfg.timer;
+        serviceConfig.ExecStart = "${pkgs.curl}/bin/curl ${url}";
+      })));
+in {
+  options.services.dns-records-update = {
+    enable = mkEnableOption "Automatically update DNS records";
+    timer = mkOption {
+      type = types.string;
+      default = "01:00:00";
+      description = "Interval at which the service is run (systemd time)";
+    };
+    urls = mkOption {
+      type = types.attrsOf types.string;
+      default = {};
+      description = "URLs to contact when service is run";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.services = createServices cfg.urls;
+  };
+}
