eca98e9be3ac42549028a544b3aba8d37fb14bed
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2019-01-24 23:21:09 +0100

    Nixify vpn configurations

--- plugables/openvpn/serverlist.nix
+++ plugables/openvpn/serverlist.nix
@@ -1,20 +1,33 @@
 { config, pkgs, lib, ... }:
+let
+  mkDir = (path:
+  (pkgs.stdenv.mkDerivation {
+    name = "fake-name";
+    src = path;
+    installPhase = ''
+    mkdir $out
+    cp * $out -r
+    '';
+  }));
+
+  mullvad_dir = (mkDir ../../private/openvpn/mullvad);
+in
 {
   # Add OpenVPN servers, currently only works on one system
   services.openvpn.servers = {
     mullvadAT = { 
-      config = builtins.readFile ../../sources/mutable/private/openvpn/mullvad/mullvad_at.conf;
-      updateResolvConf = true;
-      autoStart = false;
-    };
-    mullvadHU = { 
-      config = builtins.readFile ../../sources/mutable/private/openvpn/mullvad/mullvad_hu.conf;
+      config = ''
+        config ${mullvad_dir}/mullvad_at.conf
+        auth-user-pass ${mullvad_dir}/mullvad_userpass.txt
+        ca ${mullvad_dir}/mullvad_ca.crt
+        crl-verify ${mullvad_dir}/mullvad_crl.pem
+      '';
       updateResolvConf = true;
       autoStart = false;
     };
   };
 
-  powerManagement.resumeCommands = ''
-  PATH=$PATH:${pkgs.systemd}/bin:${pkgs.gnugrep}/bin:${pkgs.bash}/bin ${./.}/restart-openvpn.sh
-  '';
+  # powerManagement.resumeCommands = ''
+  # PATH=$PATH:${pkgs.systemd}/bin:${pkgs.gnugrep}/bin:${pkgs.bash}/bin ${./.}/restart-openvpn.sh
+  # '';
 }
