5cfb424625537d45054ee74d6ed6e1fffa9093b5
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2019-02-10 13:24:48 +0100

    Actually commit email plugables

---
+++ plugables/email/home-manager.nix
@@ -0,0 +1,80 @@
+{ config, lib, pkgs, ... }:
+
+let
+  # This has to match "smtp_sasl_password_maps" in ./postfix-queue.nix
+  pass_map_file = "/etc/nixos/nixos-config/private/postfix/sasl_password_maps";
+  createAccount = {fakePrimary, realName, userName, address, imap }:
+  {
+    inherit realName userName address;
+    passwordCommand = ''${pkgs.gnugrep}/bin/grep "${address}" ${pass_map_file} | ${pkgs.gnugrep}/bin/grep -o "[^:]*$" ${pass_map_file}'';
+    flavor = "plain";
+    primary = fakePrimary;
+
+    inherit imap;
+
+    mbsync = {
+      enable = true;
+      create = "maildir";
+      expunge = "both";
+      flatten = ".";
+      patterns = [ "*" "!.*" ];
+    };
+    
+    msmtp = {
+      enable = false;
+    };
+
+    notmuch.enable = true;
+    
+  };
+
+  createMsmtprc = symbol: value: 
+  ''
+account ${symbol}
+auth off
+from ${value.address}
+host localhost
+port 25
+tls off
+tls_starttls off
+user ${value.userName}
+  '';
+in
+
+rec {
+  accounts.email = {
+    maildirBasePath = ".maildir";
+    accounts = {
+      outlook = createAccount (import ./../../private/mail/accounts.nix).outlook;
+    };
+  };
+
+  home.file.".msmtprc".text = lib.concatStringsSep "\n\n"
+    ((lib.mapAttrsToList createMsmtprc accounts.email.accounts) ++ ["account default : outlook"]);
+
+  programs.mbsync.enable = true;
+  programs.msmtp.enable = true;
+  
+  services.mbsync =  {
+    enable = true;
+    postExec = ''
+      ${pkgs.dash}/bin/dash -c '\
+      ${pkgs.notmuch}/bin/notmuch --config=${config.xdg.configHome}/notmuch/notmuchrc new'
+    '';
+      #${pkgs.mu}/bin/mu index --maildir=${accounts.email.maildirBasePath}'
+  };
+
+  programs.notmuch = {
+    enable = true;
+    extraConfig = {
+      maildir = {
+        synchronize_flags = "true";
+      };
+    };
+  };
+
+  home.file.".mailcap".text = ''
+      text/html; ${pkgs.w3m}/bin/w3m -dump %s; nametemplate=%s.html; copiousoutput
+    '';
+
+}
---
+++ plugables/email/postfix-queue.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, lib, ... }:
+{
+  services.postfix = {
+    enable = true;
+    config = {
+      myorigin = "$mydomain";
+      #mynetworks = 127.0.0.0/8
+      mynetworks_style = "host";
+
+      smtp_sender_dependent_authentication = true;
+      #relayhost = "[smtp.outlook.com]:587";
+      #smtp_sasl_password_maps = "hash:/etc/nixos/nixos-config/private/postfix/sasl_password_map_default";
+      # This has to match "pass_map_file" in ./home-manager.nix
+      smtp_sasl_password_maps = "hash:/etc/nixos/nixos-config/private/postfix/sasl_password_maps";
+      sender_dependent_relayhost_maps = "hash:/etc/nixos/nixos-config/private/postfix/sender_relay_maps";
+
+      # SASL SUPPORT FOR SERVERS
+      #
+      # The following options set parameters needed by Postfix to enable
+      # Cyrus-SASL support for authentication of mail servers.
+      
+      smtp_always_send_ehlo = true;
+      smtp_sasl_auth_enable = "yes";
+      smtp_sasl_security_options = "noanonymous";
+      smtp_sasl_mechanism_filter = "plain, login";
+      
+      smtpd_sasl_auth_enable = "no";
+      #smtpd_sasl_path = "smtpd";
+      #smtpd_sasl_security_options = "noanonymous";
+      #smtpd_sasl_password_maps = "hash:/path/to/file";
+      #smtpd_sasl_mechanism_filter = "plain, login";
+
+      smtp_use_tls = "yes";
+      smtp_tls_security_level = "encrypt";
+      smtpd_use_tls = "no";
+
+    }; 
+  }; 
+}
