5c891b7fe68884d7cca1640d95aef0a6209bee32
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2019-04-29 04:31:34 +0200

    Commit wireguard config

---
+++ plugables/wireguard/wg-quick.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }:
+let
+  wg = (import ./mullvad.nix pkgs).networking.wireguard.interfaces.wg0;
+  address = wg.ips;
+  privateKeyFile = wg.privateKeyFile;
+  peers = wg.peers;
+in
+{
+  networking.wg-quick.interfaces = {
+    wg0 = {
+
+      # postUp = ''
+      #   ${pkgs.iptables}/bin/iptables -I OUTPUT ! -o wg0 -m mark ! --mark $(wg show wg0 fwmark) -m addrtype ! --dst-type LOCAL -j REJECT &&\
+      #   ${pkgs.iptables}/bin/ip6tables -I OUTPUT ! -o wg0 -m mark ! --mark $(wg show wg0 fwmark) -m addrtype ! --dst-type LOCAL -j REJECT
+      # '';
+      # preDown = ''
+      #   ${pkgs.iptables}/bin/iptables -D OUTPUT ! -o wg0 -m mark ! --mark $(wg show wg0 fwmark) -m addrtype ! --dst-type LOCAL -j REJECT &&\
+      #   ${pkgs.iptables}/bin/ip6tables -D OUTPUT ! -o wg0 -m mark ! --mark $(wg show wg0 fwmark) -m addrtype ! --dst-type LOCAL -j REJECT
+      # '';
+
+      dns = [ "193.138.218.74" ];
+      inherit address privateKeyFile peers;
+    };
+  };
+}
