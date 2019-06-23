b2a606a861119e2ac4e1d6aa3e39a4768fe5cdb2
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2019-06-23 22:49:57 +0200

    ran nixfmt on deployments

--- deployments/fujitsu.nix
+++ deployments/fujitsu.nix
@@ -2,139 +2,133 @@
   network.description = "Fujitsu Server";
   network.enableRollback = true;
 
-  nixos-fujitsu =
-    { config, pkgs, ... }:
-    { 
-      imports = [
-        ../hardware/fujitsu.nix
-        # ../plugables/wireguard/mullvad.nix
-        ../modules/dns-records.nix
-        ../plugables/avahi/default.nix
-        ../plugables/znc/default.nix
-        ../private/dns.nix
-        ../sources/external/clever/qemu.nix
-      ];
+  nixos-fujitsu = { config, pkgs, ... }: {
+    imports = [
+      ../hardware/fujitsu.nix
+      # ../plugables/wireguard/mullvad.nix
+      ../modules/dns-records.nix
+      ../plugables/avahi/default.nix
+      ../plugables/znc/default.nix
+      ../private/dns.nix
+      ../sources/external/clever/qemu.nix
+    ];
 
-      qemu-user.aarch64 = true;
+    qemu-user.aarch64 = true;
 
-      nix.trustedUsers = [ "root" "remote-builder" ];
+    nix.trustedUsers = [ "root" "remote-builder" ];
 
-      users.extraUsers.remote-builder = {
-        isNormalUser = true;
-        shell = pkgs.bash;
-      };
+    users.extraUsers.remote-builder = {
+      isNormalUser = true;
+      shell = pkgs.bash;
+    };
 
-      environment.systemPackages = with pkgs; [
-        htop
-        ncdu
-        nix-top
-        vim
-        speedtest-cli
-        fd
-        ripgrep
-        iperf
-      ];
+    environment.systemPackages = with pkgs; [
+      htop
+      ncdu
+      nix-top
+      vim
+      speedtest-cli
+      fd
+      ripgrep
+      iperf
+    ];
 
-      services.nginx = {
-        enable = true;
-        package = pkgs.nginxMainline;
+    services.nginx = {
+      enable = true;
+      package = pkgs.nginxMainline;
 
-        recommendedGzipSettings = true;
-        recommendedOptimisation = true;
-        recommendedProxySettings = true;
-        recommendedTlsSettings = true;
-        
-        virtualHosts = {
-          "le0.gs" = {
-            enableACME = true;
-            #useACMEHost = "le0.gs";
-            #addSSL = true;
-            forceSSL = true;
-            locations = {
-              "/".root = "${pkgs.callPackage ./site/default.nix {}}/";
-              "/rustdoc" = {
-                root = "/var/web/";
-                index = "settings.html";
-              };
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
 
-              #extraConfig = "autoindex on;";
+      virtualHosts = {
+        "le0.gs" = {
+          enableACME = true;
+          #useACMEHost = "le0.gs";
+          #addSSL = true;
+          forceSSL = true;
+          locations = {
+            "/".root = "${pkgs.callPackage ./site/default.nix { }}/";
+            "/rustdoc" = {
+              root = "/var/web/";
+              index = "settings.html";
             };
-          };
-          "test.le0.gs" = {
-            enableACME = true;
-            addSSL = true;
-            globalRedirect = "vwa.le0.gs";
-          };
-          "vwa.le0.gs" = {
-            enableACME = true;
-            forceSSL = true;
-            locations = {
-              "/".root = "/var/web/hugo/public";
-            };
-          };
-          "restic.le0.gs" = {
-            enableACME = true;
-            forceSSL = true;
-            locations = {
-              "/".root = "/var/web/restic";
-            };
-          };
-          "stats.le0.gs" = {
-            enableACME = true;
-            forceSSL = true;
-            locations = {
-              "/".proxyPass = "http://localhost:19999/";
-            };
+            #extraConfig = "autoindex on;";
           };
         };
+        "test.le0.gs" = {
+          enableACME = true;
+          addSSL = true;
+          globalRedirect = "vwa.le0.gs";
+        };
+        "vwa.le0.gs" = {
+          enableACME = true;
+          forceSSL = true;
+          locations = { "/".root = "/var/web/hugo/public"; };
+        };
+        "restic.le0.gs" = {
+          enableACME = true;
+          forceSSL = true;
+          locations = { "/".root = "/var/web/restic"; };
+        };
+        "stats.le0.gs" = {
+          enableACME = true;
+          forceSSL = true;
+          locations = { "/".proxyPass = "http://localhost:19999/"; };
+        };
       };
-
-      # security.acme.certs = {
-      #   "le0.gs" = { 
-      #     email = "leo.gaskin@brg-feldkirchen.at";
-      #     webroot = "/var/lib/acme/acme-challenges";
-      #     extraDomains = {
-      #       "le0.gs" = null;
-      #       "test.le0.gs" = null;
-      #     };
-      #     postRun = "systemctl restart nginx.service";
-      #   };
-      # };
-      
-      services.restic.server = {
-        enable = true;
-        prometheus = true;
-      };
-
-      services.netdata.enable = true;
-
-      services.haveged.enable = true;
-
-      # udisks depends on gtk+ which I don't want on my headless servers
-      services.udisks2.enable = false;
-
-      services.openssh.enable = true;
-      services.openssh.permitRootLogin = "yes";
-
-      services.avahi.enable = true;
-
-      networking.firewall.enable = true;
-      networking.firewall.allowedTCPPorts = [
-        # ssh
-        22
-        # http(s)
-        80 443
-        # stuff
-        6667
-        8000
-        666
-        8384
-      ];
-
-      deployment.targetHost = "nixos-fujitsu.local";
-      # deployment.targetHost = "192.168.178.40";
-       
-      nixpkgs.config.allowUnfree = true;
-      nixpkgs.config.allowBroken = false;
     };
+
+    # security.acme.certs = {
+    #   "le0.gs" = { 
+    #     email = "leo.gaskin@brg-feldkirchen.at";
+    #     webroot = "/var/lib/acme/acme-challenges";
+    #     extraDomains = {
+    #       "le0.gs" = null;
+    #       "test.le0.gs" = null;
+    #     };
+    #     postRun = "systemctl restart nginx.service";
+    #   };
+    # };
+
+    services.restic.server = {
+      enable = true;
+      prometheus = true;
+    };
+
+    services.syncthing.enable = true;
+
+    services.netdata.enable = true;
+
+    services.haveged.enable = true;
+
+    # udisks depends on gtk+ which I don't want on my headless servers
+    services.udisks2.enable = false;
+
+    services.openssh.enable = true;
+    services.openssh.permitRootLogin = "yes";
+
+    services.avahi.enable = true;
+
+    networking.firewall.enable = true;
+    networking.firewall.allowedTCPPorts = [
+      # ssh
+      22
+      # http(s)
+      80
+      443
+      # stuff
+      6667
+      8000
+      666
+      8384
+    ];
+
+    deployment.targetHost = "nixos-fujitsu.local";
+    # deployment.targetHost = "192.168.178.40";
+
+    nixpkgs.config.allowUnfree = true;
+    nixpkgs.config.allowBroken = false;
+  };
 }
--- deployments/rpi.nix
+++ deployments/rpi.nix
@@ -2,64 +2,57 @@
   network.description = "RPI3 Server";
   network.enableRollback = true;
 
-  nixos-rpi =
-    { config, pkgs, ... }:
-    { 
-      imports = [
-        ../hardware/raspberry3.nix
-        ../plugables/avahi/default.nix
-      ];
+  nixos-rpi = { config, pkgs, ... }: {
+    imports = [ ../hardware/raspberry3.nix ../plugables/avahi/default.nix ];
 
-      nix.trustedUsers = [ "root" "remote-builder" ];
+    nix.trustedUsers = [ "root" "remote-builder" ];
 
-      users.extraUsers.remote-builder = {
-        isNormalUser = true;
-        shell = pkgs.bash;
-      };
+    users.extraUsers.remote-builder = {
+      isNormalUser = true;
+      shell = pkgs.bash;
+    };
 
-      environment.systemPackages = with pkgs; [
-        htop
-        ncdu
-        nix-top
-        speedtest-cli
-        iperf
-      ];
+    environment.systemPackages = with pkgs; [
+      htop
+      ncdu
+      nix-top
+      speedtest-cli
+      iperf
+    ];
 
-      # netdata monitoring
-      services.nginx = {
-        enable = true;
-        package = pkgs.nginxMainline;
+    # netdata monitoring
+    services.nginx = {
+      enable = true;
+      package = pkgs.nginxMainline;
 
-        recommendedGzipSettings = true;
-        recommendedOptimisation = true;
-        recommendedProxySettings = true;
-        recommendedTlsSettings = true;
-        
-        virtualHosts = {
-          "localhost" = {
-            locations = {
-              "/".proxyPass = "http://localhost:19999/";
-            };
-          };
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+
+      virtualHosts = {
+        "localhost" = {
+          locations = { "/".proxyPass = "http://localhost:19999/"; };
         };
       };
-
-      services.netdata.enable = true;
-
-      # udisks depends on gtk+ which I don't want on my headless servers
-      services.udisks2.enable = false;
-
-      services.openssh.enable = true;
-      services.openssh.permitRootLogin = "yes";
-
-      networking.firewall.enable = true;
-      networking.firewall.allowedTCPPorts = [ 22 80 443 ];
-
-      deployment.targetHost = "nixos-rpi.local";
-      #deployment.targetHost = "192.168.178.23";
-
-      #boot.kernelPackages = pkgs.linuxPackages_latest;
-      nixpkgs.system = "aarch64-linux";
-      nixpkgs.config.allowBroken = false;
     };
+
+    services.netdata.enable = true;
+
+    # udisks depends on gtk+ which I don't want on my headless servers
+    services.udisks2.enable = false;
+
+    services.openssh.enable = true;
+    services.openssh.permitRootLogin = "yes";
+
+    networking.firewall.enable = true;
+    networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+
+    deployment.targetHost = "nixos-rpi.local";
+    #deployment.targetHost = "192.168.178.23";
+
+    #boot.kernelPackages = pkgs.linuxPackages_latest;
+    nixpkgs.system = "aarch64-linux";
+    nixpkgs.config.allowBroken = false;
+  };
 }
