5a619002490083022aaea01159a9606f5d8b20ca
leotaku <leo.gaskin@brg-feldkirchen.at>
2019-10-25 00:15:52 +0200

    Make avahi plugable more explicit

--- plugables/avahi/default.nix
+++ plugables/avahi/default.nix
@@ -2,6 +2,7 @@
 {
   services.avahi = {
     enable = true;
+    domainName = "local";
     nssmdns = true;
     publish = {
       enable = true;
@@ -9,5 +10,13 @@
       userServices = true;
       addresses = true;
     };
+  };
+
+  # Doesn't do anything, because of mdns minimal
+  environment.etc."mdns.allow" = {
+    enable = false;
+    text = ''
+      .local
+    '';
   };
 }
