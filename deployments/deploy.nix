c93afe616a9636ffc72d9625639a33df7b8100b5
leotaku <leo.gaskin@brg-feldkirchen.at>
2019-10-25 00:14:22 +0200

    New morph deployment file suitable for custom fork

---
+++ deployments/deploy.nix
@@ -0,0 +1,20 @@
+let
+  pinned = import ../sources/links/nixos-19_09 {};
+in {
+  network = {
+    description = "Home network";
+    overlays = [];
+    config = {
+      allowUnfree = true;
+      allowBroken = false;
+    };
+    pkgs = pinned;
+  };
+} // {
+  "nixos-laptop.local" = {
+    configuration = (import ./laptop/configuration.nix);
+    nixpkgs = ../sources/links/nixos-unstable;
+  };
+  "nixos-fujitsu.local".configuration = (import ./fujitsu.nix);
+  "nixos-rpi.local".configuration = (import ./rpi.nix);
+}
