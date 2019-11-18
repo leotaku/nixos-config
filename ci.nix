73893b8d374c658e230662ce422596271ab704f8
leotaku <leo.gaskin@brg-feldkirchen.at>
2019-11-18 21:17:52 +0100

    Update ci.nix to also build RPI system

--- ci.nix
+++ ci.nix
@@ -1,15 +1,26 @@
 let
   sources = import ./sources/nix/sources.nix;
   # TODO: build for other nixpkgs versions
-  nixos-unstable = import sources.nixos-unstable;
-  pkgs = nixos-unstable {
+  pkgs = import nixos-unstable {
     overlays = [ (import ./pkgs/default.nix) ];
   };
+  pkgs-stable = import nixos-19_09 {
+    overlays = [ (import ./pkgs/default.nix) ];
+  };
+  inherit (sources) nixos-19_09 nixos-unstable;
+
+  filter = file: a @ { config, pkgs, lib, ... }:
+    builtins.removeAttrs (import file a) [ "deployment" ];
 in
 {
   x86_64-linux = pkgs.recurseIntoAttrs (
     # Build packages defined in overlays
-    import ./pkgs/default.nix pkgs pkgs
+    (import ./pkgs/default.nix pkgs pkgs) // {
+      # Build my Raspberry PI 3 system
+      rpi = (import "${pkgs-stable.path}/nixos" {
+        configuration = filter ./deployments/rpi.nix;
+      }).system;
+    }
   );
 }
 
