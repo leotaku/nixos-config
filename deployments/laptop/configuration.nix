eda1f27070aa05d296c7ce2b4e5ea0c61f854e3e
leotaku <leo.gaskin@brg-feldkirchen.at>
2019-12-03 20:48:09 +0100

    Formatting stuff

--- deployments/laptop/configuration.nix
+++ deployments/laptop/configuration.nix
@@ -2,6 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 { config, pkgs, ... }:
+
 {
   imports = with (import ../../sources/nix/sources.nix); [ 
     # Import configuration
--- deployments/laptop/system.nix
+++ deployments/laptop/system.nix
@@ -3,7 +3,8 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, lib, ... }:
-{ 
+
+{
   imports = [
     # Import plugable configurations
     ../../plugables/avahi/module.nix
