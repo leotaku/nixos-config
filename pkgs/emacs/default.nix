a5ba33179d5398329abd784e5559e751a1b66158
leotaku <leo.gaskin@brg-feldkirchen.at>
2020-01-27 19:47:05 +0100

    Improve emacs-custom with paths

--- pkgs/emacs/default.nix
+++ pkgs/emacs/default.nix
@@ -1,24 +1,27 @@
 { pkgs, emacsPackagesNgGen, emacs, imagemagick, ... }:
 let
-  customEmacsPackages = emacsPackagesNgGen (
-    (emacs.override {
-      inherit imagemagick;
-      withX = true;
-      withGTK3 = true;
-      withXwidgets = true;
-    }).overrideAttrs (oldAttrs: {
-      configureFlags = oldAttrs.configureFlags ++ [
-        "--without-toolkit-scroll-bars"
-      ];
-    })
-  );
-in customEmacsPackages.emacsWithPackages (epkgs: [
-  # Native Emacs packages
-  epkgs.closql
-  epkgs.editorconfig
-  epkgs.forge
-  epkgs.jupyter
-  epkgs.vterm
-  epkgs.zmq
-  pkgs.notmuch
-])
+  customEmacsPackages = emacsPackagesNgGen ((emacs.override {
+    inherit imagemagick;
+    withX = true;
+    withGTK3 = true;
+    withXwidgets = true;
+  }).overrideAttrs (oldAttrs: {
+    configureFlags = oldAttrs.configureFlags
+      ++ [ "--without-toolkit-scroll-bars" ];
+  }));
+  emacs-with-packages = customEmacsPackages.emacsWithPackages (epkgs: [
+    # Native Emacs packages
+    epkgs.closql
+    epkgs.editorconfig
+    epkgs.forge
+    epkgs.jupyter
+    epkgs.vterm
+    epkgs.zmq
+    pkgs.notmuch
+  ]);
+  emacs-with-paths = pkgs.symlinkJoin {
+    name = builtins.replaceStrings [ "emacs-with-packages" ] [ "emacs-custom" ]
+      emacs-with-packages.name;
+    paths = [ emacs-with-packages pkgs.editorconfig-core-c ];
+  };
+in emacs-with-paths
