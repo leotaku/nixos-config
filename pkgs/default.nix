3d1aa4e855a479a9ef019dd25cfa952a3b67aa8d
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-03-07 00:30:09 +0100

    Improve pkgs with with and nixfmt

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,18 +1,16 @@
-self: super:
-
-let
-  sources = import ../sources/nix/sources.nix;
-in
-{
+let sources = import ../sources/nix/sources.nix;
+in self: super:
+with self; {
   # Custom packages
-  alegreya = super.callPackage ./alegreya/default.nix { };
-  besley = super.callPackage ./besley/default.nix { };
-  catimg = super.callPackage ./catimg/default.nix { };
-  chroma = super.callPackage ./chroma/default.nix { };
-  gitbatch = super.callPackage ./gitbatch/default.nix { };
-  n30f = super.callPackage ./n30f/default.nix { };
-  sddm-themes = super.callPackage ./sddm/default.nix { };
-  zplugin-install = super.callPackage ./zplugin/install.nix { };
+  alegreya = callPackage ./alegreya/default.nix { };
+  besley = callPackage ./besley/default.nix { };
+  catimg = callPackage ./catimg/default.nix { };
+  chroma = callPackage ./chroma/default.nix { };
+  gitbatch = callPackage ./gitbatch/default.nix { };
+  n30f = callPackage ./n30f/default.nix { };
+  sddm-themes = callPackage ./sddm/default.nix { };
+  zplugin = callPackage ./zplugin/default.nix { };
+  zplugin-install = callPackage ./zplugin/install.nix { };
 
   # Customized packages
   ncmpcpp = super.ncmpcpp.override {
@@ -25,7 +23,7 @@ in
     mpdSupport = true;
   };
   sddm = super.sddm.overrideAttrs (oldAttrs: {
-    buildInputs = with super.qt5;
+    buildInputs = with qt5;
       oldAttrs.buildInputs ++ [ qtgraphicaleffects qtmultimedia ];
   });
   rxvt-unicode-custom = super.rxvt_unicode_with-plugins.override {
@@ -36,18 +34,19 @@ in
   wireguard-tools =
     super.wireguard-tools.override { openresolv = self.systemd; };
 
-  # Emacs
-  emacs-git = super.callPackage ./emacs/emacs-git.nix { };
-  emacs-custom = super.callPackage ./emacs/default.nix { };
-  emacs-git-custom = super.callPackage ./emacs/default.nix {
-    emacs = self.emacs-git;
   };
+  # Emacs
+  emacs-git = callPackage ./emacs/emacs-git.nix { };
+  emacs-custom = callPackage ./emacs/default.nix { };
+  emacs-git-custom =
+    callPackage ./emacs/default.nix { emacs = self.emacs-git; };
+  ox-blorf = callPackage ./emacs/ox-blorf.nix { };
 
   # External
   mozilla = import sources.nixpkgs-mozilla self super;
   pboy = import sources.pboy;
-  morph = super.callPackage (sources.morph + "/nix-packaging") { };
+  morph = callPackage (sources.morph + "/nix-packaging") { };
   haskell = super.haskell // {
-    compiler = super.callPackage sources.old-ghc-nix {} // super.haskell.compiler;
+    compiler = callPackage sources.old-ghc-nix { } // super.haskell.compiler;
   };
 }
