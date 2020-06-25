9f138a8540b3c1d7cf94bfb811ef4856d3e909ca
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-06-25 12:58:39 +0200

    Simplify CI configuration

--- ci.nix
+++ ci.nix
@@ -1,23 +1,10 @@
 let
   sources = import ./sources/nix/sources.nix;
-  # TODO: build for other nixpkgs versions
-  pkgs = import nixos-unstable {
-    overlays = [ (import ./pkgs/default.nix) ];
-  };
-  pkgs-stable = import nixos-19_09 {
-    overlays = [ (import ./pkgs/default.nix) ];
-  };
-  inherit (sources) nixos-19_09 nixos-unstable;
-
-  filter = file: a @ { config, pkgs, lib, ... }:
-    builtins.removeAttrs (import file a) [ "deployment" ];
-in
-{
+  pkgs = import sources.nixos-unstable { };
+in {
   x86_64-linux = pkgs.recurseIntoAttrs (
     # Build packages defined in overlays
-    (import ./pkgs/default.nix pkgs pkgs) // {
-      # Other stuff
-    }
+    import ./pkgs/default.nix pkgs pkgs
   );
 }
 
