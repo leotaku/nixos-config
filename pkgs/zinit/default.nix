470b0c985e557515ee15c59e2b2366a097680a10
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-06-25 12:46:51 +0200

    Update pkgs

--- pkgs/zinit/default.nix
+++ pkgs/zinit/default.nix
@@ -7,8 +7,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     repo = "zinit";
     owner = "zdharma";
-    rev = "3a1576d362607c5aa8d6f30e485c20c8ecc726fc";
-    sha256 = "1y3dwpnh2asd2zlhinblx45pl02i28a281yznpvbixkckrs6xjm4";
+    rev = "e53c6878b96161a2fea4c94291019ffbe32bf3d2";
+    sha256 = "0sdq9pvdgimfw0j068zmgfg0px8jxsyziqc5c4ls48jdb3r2aygz";
   };
 
   buildInputs = [ zsh gnumake ncurses autoconf gcc ];
