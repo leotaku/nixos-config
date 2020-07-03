7268e8dc5dad4e233b85fc1bdcd3b2c1792dd7dd
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-07-03 23:51:48 +0200

    Simplify and update niv machinery

--- sources/link.sh
+++ sources/link.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 dir="$(dirname $(realpath $0))"
 linkdir="${dir}/links"
-if ! [[ -d "$linkdir"  ]]; then
-    mkdir "$linkdir"
-fi
 
-names=`nix-instantiate --json --eval -E "builtins.attrNames (import ./nix/sources.nix)" | jq -r '.[]'`
+if [[ -d "$linkdir"  ]]; then
+    rm -r "$linkdir"
+fi
+mkdir "$linkdir"
+
+nix-eval() {
+    nix-instantiate --json --eval -E "$1" | jq -r "$2"
+}
+
+names=$(nix-eval 'builtins.attrNames (import ./nix/sources.nix)' '.[]')
 
 while read name; do
-    {
-        output=`nix build -f "${dir}/nix/sources.nix" "$name" --out-link "${linkdir}/${name}"`
-        echo "key: $name"
-        [ -n "$output" ] && echo "output: $output"
-    } &
-done <<< $names
+    if [[ "${name}" != "__functor" ]]; then
+        output=$(nix-eval "(import ./nix/sources.nix).${name}.outPath" '.')
+        target="${linkdir}/${name}"
 
-wait $(jobs -p)
-exit 0
+        if [[ -n "${output}" ]]; then
+            echo "  Package: ${name}"
+            ln -s "$output" "$target"
+            nix-store --add-root "$target" --indirect -r "$output"
+        fi
+    fi
+done <<< $names
--- sources/update.sh
+++ sources/update.sh
@@ -3,33 +3,10 @@ dir="$(dirname $(realpath $0))"
 linkdir="${dir}/links"
 cd "$dir"
 
-# functions
-jq_act () {
-    cat "$1" | jq -r '[[keys_unsorted, [.[]]] | transpose[] | (.[0] + ": " + .[1].rev)] | .[]'
-}
-
 # fetch
 echo -e "\033[2;42m\033[2;30mfetching:\033[0m"
-old="$(mktemp)"
-jq_act "${dir}/nix/sources.json" > $old
-niv update
-
-# test if relinking (or force)
-if [[ -z "$1" ]]; then
-    diff "$old" <(jq_act ${dir}/nix/sources.json) --brief >/dev/null
-    [[ "$?" == "0" ]] && exit 0
-fi
-
-# show changes
-echo -e "\033[2;41m\033[2;30mchanges:\033[0m"
-diff "$old" <(jq_act ${dir}/nix/sources.json) --color=auto
+niv update || exit 1
 
 # relink
 echo -e "\033[2;43m\033[2;30mlinking:\033[0m"
-"${dir}/link.sh"
-
-# cleanup
-cleanup () {
-    [[ -n "$old" ]] && rm "$old"
-}
-trap cleanup 0 1 2 3 6
+"${dir}/link.sh" || exit 1
