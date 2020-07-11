3d9ed8909da19abdaa51c449344b421d73cd029c
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-07-11 11:21:56 +0200

    Improve link script

--- sources/link.sh
+++ sources/link.sh
@@ -15,11 +15,11 @@ names=$(nix-eval 'builtins.attrNames (import ./nix/sources.nix)' '.[]')
 
 while read name; do
     if [[ "${name}" != "__functor" ]]; then
+        echo "  Package: ${name}"
         output=$(nix-eval "(import ./nix/sources.nix).${name}.outPath" '.')
         target="${linkdir}/${name}"
 
         if [[ -n "${output}" ]]; then
-            echo "  Package: ${name}"
             ln -s "$output" "$target"
             nix-store --add-root "$target" --indirect -r "$output"
         fi
