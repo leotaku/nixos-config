787f8c0036722d837fe11d7e8d0dd6608f63c038
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-08-18 06:09:12 +0200

    Fix Nvidia settings

--- hardware/thinkpad-t480s.nix
+++ hardware/thinkpad-t480s.nix
@@ -28,7 +28,7 @@
   environment.systemPackages = with config.boot.kernelPackages.nvidia_x11; [ bin settings ];
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.prime = {
-    enable = true;
+    offload.enable = true;
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
   };
