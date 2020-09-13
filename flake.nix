0e0c5ace3f23b38ae2aa25a08e513909e2b1d771
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-09-13 11:04:51 +0200

    Add initial Flake setup

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "My overlays and local system";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      final = self.legacyPackages.x86_64-linux;
+      prev = import nixpkgs {
+        config.allowUnfree = true;
+        system = "x86_64-linux";
+      };
+    in {
+      # Systems
+      nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ (import ./deployments/laptop/configuration.nix) ];
+      };
+
+      # Custom overlay
+      overlay = import ./pkgs/default.nix;
+
+      # Package set with overlay
+      legacyPackages.x86_64-linux = prev // (self.overlay final prev);
+    };
+}
