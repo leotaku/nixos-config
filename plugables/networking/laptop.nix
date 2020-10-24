78d51828b112acc6bdde9473acea784cba04bf27
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-10-24 10:39:46 +0200

    Switch from connman to systemd-networkd for better stability

--- plugables/networking/laptop.nix
+++ plugables/networking/laptop.nix
@@ -3,32 +3,28 @@
 {
   imports = [ ./shared.nix ];
 
-  # Networking
-  services.connman = {
-    enable = true;
-    wifi.backend = "wpa_supplicant";
-    networkInterfaceBlacklist =
-      [ "docker" "vmnet" "vboxnet" "virbr" "ifb" "ve" "vnet" "eth" "wlan" ];
-    extraFlags = [ "--nodnsproxy" ];
-    extraConfig = ''
-      [General]
-      PreferredTechnologies=ethernet,wifi
-    '';
-  };
+  # Use systemd-networkd for networking
+  networking.useNetworkd = true;
   networking.useDHCP = false;
 
-  # Networking GUI
-  systemd.user.services."connman-gtk" = {
-    enable = true;
-    serviceConfig = {
-      ExecPreStart = pkgs.coreutils + "/bin/sleep 3";
-      ExecStart = pkgs.connman-gtk + "/bin/connman-gtk --tray";
-      partOf = "graphical-session.target";
-      Type = "simple";
+  # Setup all wired network interfaces
+  systemd.network.networks."40-wired" = {
+    matchConfig = { Name = lib.mkForce "enp* eth*"; };
+    networkConfig = {
+      DHCP = "yes";
     };
-    wantedBy = [ "default.target" ];
-    after = [ "graphical-session.target" "network.target" ];
   };
+
+  # Wireless
+  networking.wireless.iwd.enable = true;
+  environment.etc."iwd/main.conf".text = ''
+    [General]
+    EnableNetworkConfiguration=true
+    UseDefaultInterface=true
+    [Network]
+    NameResolvingService=systemd
+    RoutePriorityOffset=2048
+  '';
 
   # Enable Avahi
   services.avahi = {
