d78a6a92b8eb0e2f340f33a151ae1c1709a311ab
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-11-26 15:29:56 +0100

    Improve and fix custom xkeyboard configuration

--- plugables/xkeyboard/module.nix
+++ plugables/xkeyboard/module.nix
@@ -6,39 +6,5 @@
     xkbVariant = "leo";
   };
 
-  nixpkgs.config = {
-    packageOverrides = super: rec {
-
-      xorg = super.xorg // rec {
-        xkeyboardconfig-custom = super.xorg.xkeyboardconfig.overrideAttrs
-          (oldAttrs: { patches = [ ./custom.patch ]; });
-
-        xorgserver = super.xorg.xorgserver.overrideAttrs (oldAttrs: {
-          configureFlags = oldAttrs.configureFlags ++ [
-            "--with-xkb-bin-directory=${xkbcomp}/bin"
-            "--with-xkb-path=${xkeyboardconfig-custom}/share/X11/xkb"
-          ];
-        });
-
-        setxkbmap = super.xorg.setxkbmap.overrideAttrs (oldAttrs: {
-          postInstall = ''
-            mkdir -p $out/share
-            ln -sfn ${xkeyboardconfig-custom}/etc/X11 $out/share/X11
-          '';
-        });
-
-        xkbcomp = super.xorg.xkbcomp.overrideAttrs (oldAttrs: {
-          configureFlags =
-            "--with-xkb-config-root=${xkeyboardconfig-custom}/share/X11/xkb";
-        });
-
-      };
-
-      xkbvalidate = super.xkbvalidate.override {
-        libxkbcommon = super.libxkbcommon.override {
-          xkeyboard_config = xorg.xkeyboardconfig-custom;
-        };
-      };
-    };
-  };
+  nixpkgs.overlays = [ (import ./default.nix) ];
 }
