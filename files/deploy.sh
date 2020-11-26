09371437382f94715eab1dbcebbc05d8014c5cbb
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-11-26 15:35:59 +0100

    Fix and improve deploy script

--- files/deploy.sh
+++ files/deploy.sh
@@ -3,17 +3,8 @@ set -e
 MACHINE="$1"
 ACTION="$2"
 SSH_TARGET="root@nixos-${MACHINE}.local"
-ATTR="self#nixosConfigurations.${MACHINE}.config.system.build.toplevel"
+ATTRIBUTE="nixosConfigurations.${MACHINE}.config.system.build.toplevel"
 
 echo "Building machine ${MACHINE}..."
-
-if [ "$ACTION" = "build" ]; then
-    nix build --print-build-logs --no-link "$ATTR"
-else
-    echo "Copying files to remote machine..."
-    nix copy --to ssh://"$SSH_TARGET" "$ATTR"
-
-    echo "Applying configuration..."
-    STORE_PATH=$(nix path-info "$ATTR")
-    ssh "$SSH_TARGET" "$STORE_PATH"/bin/switch-to-configuration "$ACTION"
-fi
+nix copy --to ssh://"$SSH_TARGET" .#"$ATTRIBUTE"
+nixos-rebuild "$ACTION" --flake .#"$MACHINE" --target-host "$SSH_TARGET"
