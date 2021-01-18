11a1e277c802f07c44dd0a944a16c077d0c5e0c9
Leo Gaskin <leo.gaskin@le0.gs>
2021-01-19 00:41:04 +0100

    Restructure ZNC configuration

--- plugables/znc.nix
+++ plugables/znc.nix
@@ -3,39 +3,34 @@
 {
   services.znc = {
     enable = true;
+    useLegacyConfig = false;
     mutable = true;
-    openFirewall = true;
-    confOptions = {
-      # Basics
-      port = 6667;
-      useSSL = false;
-      nick = "leotaku";
-      userName = "leotaku";
-      passBlock = ''
-      <Pass password>
-        Method = sha256
-        Hash = 4d1f02701e27fee1405d52527bc93f9ad8e233a0946f1bc86ea540edeb176af7
-        Salt = 9vpyWS6!(5wkCR3uv:_5
-       </Pass>
-      '';
-      # Networks
-      networks = {
-        freenode = {
-          server = "irc.freenode.net";
-          useSSL = true;
-          modules = [ "sasl" ];
-        };
-        irchighway = {
-          server = "irc.irchighway.net";
-          useSSL = true;
-        };
-
+    config = {
+      LoadModule = [ "adminlog" ];
+      MaxBufferSize = 50000;
+      Listener.l = {
+        Port = 6667;
+        SSL = false;
       };
-      # Other
-      modules = [ "adminlog" "dcc" ];
-      extraZncConf = ''
-        MaxBufferSize=50000
-      '';
+      User.leotaku = {
+        LoadModule = [ "dcc" ];
+        Admin = true;
+        Nick = "leotaku";
+        Network.freenode = {
+          LoadModule = [ "sasl" ];
+          Server = "chat.freenode.net";
+          SSL = true;
+        };
+        Network.irchighway = {
+          Server = "irc.irchighway.net";
+          SSL = true;
+        };
+        Pass.password = {
+          Method = "sha256";
+          Hash = "4d1f02701e27fee1405d52527bc93f9ad8e233a0946f1bc86ea540edeb176af7";
+          Salt = "9vpyWS6!(5wkCR3uv:_5";
+        };
+      };
     };
   };
 }
