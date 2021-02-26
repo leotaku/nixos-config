4f88f80d5537a3263662adffbfa4864ba2fbaace
Leo Gaskin <leo.gaskin@le0.gs>
2021-02-26 17:18:25 +0100

    Revert "Disable Steam support"
    This reverts commit 204700ada884fd88279c1eeef4cc2b8d3dfb84f7.

--- deployments/laptop/system.nix
+++ deployments/laptop/system.nix
@@ -150,6 +150,12 @@
   hardware.pulseaudio.configFile = ../../files/pulseaudio.pa;
   hardware.pulseaudio.systemWide = false;
 
+  # Enable Steam support
+  programs.steam.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.steam-hardware.enable = true;
+
   # Enable virtualization/container technologies
   services.flatpak.enable = false;
   virtualisation.anbox.enable = false;
