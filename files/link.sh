bff74d33ad3e0a818c39a690a1e80a125605001e
Leo Gaskin <leo.gaskin@le0.gs>
2021-05-14 16:15:03 +0200

    Improve link.sh script
    Garbage collection may result in old nixpkgs checkouts never being
    cleaned up, but that should be fine for now.

--- files/link.sh
+++ files/link.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+set -e
+
 instantiate() {
     nix-instantiate \
         --eval ./inputs.nix \
@@ -13,3 +15,5 @@ STORE_PATH="$(instantiate | tr -d '"')"
 mkdir -p "$(dirname $TARGET)"
 rm "$TARGET" 2>/dev/null || true
 ln -s "$STORE_PATH" "$TARGET"
+
+nix-store --add-root "$TARGET" -r "$STORE_PATH"
