aabf560237d10ab5e92e4dc5999d9d494fa8a313
Leo Gaskin <leo.gaskin@le0.gs>
2021-09-19 11:32:49 +0200

    Simplify fleet module

--- modules/fleet.nix
+++ modules/fleet.nix
@@ -15,8 +15,6 @@ let
     if name == localHostName then
       null
     else {
-      sshUser = "fleet-builder";
-      sshKey = "/home/fleet-builder/.ssh/id_rsa";
       inherit hostName systems maxJobs speedFactor;
       inherit supportedFeatures mandatoryFeatures;
     };
@@ -69,45 +67,10 @@ in {
   };
 
   config = lib.mkIf (cfg.enable == true) ({
-    users.users.fleet-builder = {
-      isNormalUser = true;
-      extraGroups = [ ];
-    };
     nix = {
-      trustedUsers = [ "fleet-builder" ];
       distributedBuilds = true;
       buildMachines = buildMachines cfg.machines;
       extraOptions = "builders-use-substitutes = true";
     };
-  } // (
-    # Only offer deployment options if Morph is used
-    if hasAttr "deployment" options then {
-      deployment = {
-        targetHost = (thisMachine cfg.machines).hostName;
-        substituteOnDestination = true;
-        secrets = {
-          "fleet-builder-id_rsa" = {
-            source = "${cfg.base}/id_rsa";
-            destination = "/home/fleet-builder/.ssh/id_rsa";
-            owner.user = "fleet-builder";
-            owner.group = "nogroup";
-            action =
-              [ "chown" "fleet-builder:nogroup" "/home/fleet-builder/.ssh" ];
-          };
-          "fleet-builder-id_rsa_pub" = {
-            source = "${cfg.base}/id_rsa.pub";
-            destination = "/home/fleet-builder/.ssh/id_rsa.pub";
-            owner.user = "fleet-builder";
-            owner.group = "nogroup";
-          };
-          "fleet-builder-authorized_keys" = {
-            source = "${cfg.base}/id_rsa.pub";
-            destination = "/home/fleet-builder/.ssh/authorized_keys";
-            owner.user = "fleet-builder";
-            owner.group = "nogroup";
-          };
-        };
-      };
-    } else
-      { }));
+  });
 }
