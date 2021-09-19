4e6f87f7ce212c9cad71c12db35eecc8c8781267
Leo Gaskin <leo.gaskin@le0.gs>
2021-09-19 11:19:32 +0200

    Improve formatting

--- plugables/networking/laptop.nix
+++ plugables/networking/laptop.nix
@@ -9,29 +9,17 @@
   # Setup all wired network interfaces
   systemd.network.networks."40-wired" = {
     matchConfig = { Name = lib.mkForce "enp* eth*"; };
-    networkConfig = {
-      DHCP = "yes";
-    };
-    dhcpV4Config = {
-      RouteMetric = 200;
-    };
-    dhcpV6Config = {
-      RouteMetric = 200;
-    };
+    networkConfig = { DHCP = "yes"; };
+    dhcpV4Config = { RouteMetric = 200; };
+    dhcpV6Config = { RouteMetric = 200; };
   };
 
   # Protect all wireless networks from being unconfigured
   systemd.network.networks."40-wireless" = {
     matchConfig = { Name = lib.mkForce "wlp* wlan*"; };
-    networkConfig = {
-      DHCP = "yes";
-    };
-    dhcpV4Config = {
-      RouteMetric = 400;
-    };
-    dhcpV6Config = {
-      RouteMetric = 400;
-    };
+    networkConfig = { DHCP = "yes"; };
+    dhcpV4Config = { RouteMetric = 400; };
+    dhcpV6Config = { RouteMetric = 400; };
   };
 
   # Force predictable interface names
