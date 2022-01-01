028630755cce7450c7af7fbd6a54420755478e2d
Leo Gaskin <leo.gaskin@le0.gs>
2022-01-01 17:15:00 +0100

    Improve deploy.sh script
    Better formatting and avoid building Nix.

--- files/deploy.sh
+++ files/deploy.sh
@@ -6,7 +6,9 @@ shift
 for machine in "${@}"; do
     ssh_target="root@nixos-${machine}.local"
     echo "Building machine ${machine}..."
-    nixos-rebuild "$action" --flake .#"$machine" \
-                  --target-host "$ssh_target" \
-                  --build-host "$ssh_target"
+    nixos-rebuild "$action"         \
+        --flake .#"$machine"        \
+        --no-build-nix              \
+        --target-host "$ssh_target" \
+        --build-host "$ssh_target"
 done
