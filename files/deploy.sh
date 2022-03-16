a6322c035c7e63145d4af020085fe5b9c7644bd2
Leo Gaskin <leo.gaskin@le0.gs>
2022-03-16 13:52:51 +0100

    Revert to deploying to local host without SSH
    Using SSH for deploying to local host frequently lead to unclosed
    connections showing up.

--- files/deploy.sh
+++ files/deploy.sh
@@ -4,16 +4,20 @@ action="$1"
 shift
 
 for machine in "${@}"; do
+    echo "Building machine ${machine}..."
+
     host="nixos-${machine}.local"
     if [ "$host" == "$(hostname).local" ]; then
-        host="0.0.0.0"
+        sudo nixos-rebuild "$action"  \
+            --flake .#"$machine" \
+            --no-build-nix       \
+            --use-substitutes
+    else
+        nixos-rebuild "$action"  \
+            --flake .#"$machine" \
+            --no-build-nix       \
+            --use-substitutes    \
+            --target-host root@"$host" \
+            --build-host  root@"$host"
     fi
-
-    echo "Building machine ${machine}..."
-    nixos-rebuild "$action"  \
-        --flake .#"$machine" \
-        --no-build-nix       \
-        --use-substitutes    \
-        --target-host root@"$host" \
-        --build-host  root@"$host"
 done
