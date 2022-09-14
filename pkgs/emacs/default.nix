431d30c8804c67c99e9485507568fab116320c79
Leo Gaskin <leo.gaskin@le0.gs>
2022-09-14 20:14:14 +0200

    Fix custom Emacs package

--- pkgs/emacs/default.nix
+++ pkgs/emacs/default.nix
@@ -1,17 +1,13 @@
 { pkgs, emacsPackagesFor, emacs, ... }:
 let
-  customEmacsPackages = emacsPackagesFor ((emacs.override {
-    withX = true;
-    withGTK3 = true;
-    withXwidgets = false;
-  }).overrideAttrs (oldAttrs: {
+  customEmacsPackages = emacsPackagesFor (emacs.overrideAttrs (oldAttrs: {
     configureFlags = oldAttrs.configureFlags ++ [
       "--without-scroll-bars"
       "--without-compress-install"
       "--with-pgtk"
       "--with-sqlite3"
     ];
-    buildInputs = oldAttrs.buildInputs ++ [ pkgs.sqlite ];
+    buildInputs = oldAttrs.buildInputs ++ [ pkgs.gtk3 pkgs.sqlite ];
   }));
   emacs-with-packages = customEmacsPackages.emacsWithPackages (epkgs: [
     # Native Emacs packages
