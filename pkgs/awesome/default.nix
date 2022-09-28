70087baffa948b71ca855ef08b485c990ca24c02
Leo Gaskin <leo.gaskin@le0.gs>
2022-09-28 11:46:51 +0200

    Fix awesome package

--- pkgs/awesome/default.nix
+++ pkgs/awesome/default.nix
@@ -6,10 +6,9 @@ let
     rev = "4a140ea5ea681e7a0f62d8ef050b0ed1b905cc68";
     sha256 = "0hrfwilgd4vxhqd0hz88hq3j2rizhdlr7g0cq9q8ic0m8z47q1rq";
   };
-in
-(awesome.override {
-  gtk3Support = true;
-}).overrideAttrs (oldAttrs: {
+in (awesome.override { gtk3Support = true; }).overrideAttrs (oldAttrs: {
   inherit src;
+  cmakeFlags = oldAttrs.cmakeFlags
+    ++ [ "-DGENERATE_MANPAGES=OFF" "-DGENERATE_DOC=OFF" ];
   patches = [ ];
 })
