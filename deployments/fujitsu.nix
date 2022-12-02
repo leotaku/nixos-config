f3cbbd614966ffadaf02ae60d1d9d14c2dd24fc9
Leo Gaskin <leo.gaskin@le0.gs>
2022-12-02 09:07:03 +0100

    Add stateVersion to servers

--- deployments/fujitsu.nix
+++ deployments/fujitsu.nix
@@ -113,4 +113,10 @@
 
   # Open firewall ports
   networking.firewall.allowedTCPPorts = [ 22 80 443 8000 8384 6667 6697 25565 ];
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "23.05"; # Did you read the comment?
 }
--- deployments/rpi.nix
+++ deployments/rpi.nix
@@ -50,4 +50,10 @@
 
   # Open firewall ports
   networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "23.05"; # Did you read the comment?
 }
