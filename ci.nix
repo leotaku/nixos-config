3a40b2d40222fe06d324d9c85dab51c974eb28f1
Leo Gaskin <leo.gaskin@le0.gs>
2022-12-17 23:54:08 +0100

    Fix Hercules CI configuration

--- ci.nix
+++ ci.nix
@@ -1,8 +1,6 @@
 let
   flake = builtins.getFlake (builtins.toString ./.);
-  pkgs = import flake.inputs.nixpkgs {
-    overlays = [ (import ./pkgs/default.nix) ];
-  };
+  pkgs = flake.legacyPackages.x86_64-linux;
 in {
   x86_64-linux = pkgs.recurseIntoAttrs (
     # Build packages defined in overlays
