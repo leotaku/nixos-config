8ae6a8bb61ff0e3d90f3ec519d14b4739049607c
Leo Gaskin <leo.gaskin@le0.gs>
2022-12-28 23:01:32 +0100

    Use some additional nameserver IP addresses

--- plugables/networking/shared.nix
+++ plugables/networking/shared.nix
@@ -12,7 +12,9 @@
   # 1: https://developers.cloudflare.com/1.1.1.1/
   networking.nameservers = [
     "8.8.8.8#dns.google"
+    "8.8.4.4#dns.google"
     "1.1.1.1#1dot1dot1dot1.cloudflare-dns.com"
+    "1.0.0.1#1dot1dot1dot1.cloudflare-dns.com"
   ];
 
   # Use systemd-resolved instead of DHCPCD
