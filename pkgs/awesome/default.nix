01c90e40a14491ce80c5b040f38ca6a5544eb44f
Leo Gaskin <leo.gaskin@le0.gs>
2023-05-22 17:53:59 +0200

    Update pkgs

--- pkgs/awesome/default.nix
+++ pkgs/awesome/default.nix
@@ -3,8 +3,8 @@ let
   src = fetchFromGitHub {
     owner = "awesomeWM";
     repo = "awesome";
-    rev = "b6263bf209dc69482ddbf10987e1f71577bc98cb";
-    sha256 = "1x29rkm4vfdgg1yh5i232h629zrhy83fjrkxsixhnaw3c5xpvdzr";
+    rev = "485661b706752212dac35e91bb24a0e16a677b70";
+    sha256 = "0kp70wa5hy5v6zrn3g3d7n5idnwbgp1s3xnfz6ixnwzw8lmnlhiv";
   };
 in (awesome.override { gtk3Support = true; }).overrideAttrs (oldAttrs: {
   inherit src;
