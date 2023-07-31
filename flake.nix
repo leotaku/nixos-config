5e93c23be06efac342ab99e131b4e498800c5cce
Leo Gaskin <leo.gaskin@le0.gs>
2023-07-31 07:15:42 +0200

    Add nix-community cache to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "Leo Gaskin's overlays and personal systems";
 
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+  };
+
   inputs = {
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
